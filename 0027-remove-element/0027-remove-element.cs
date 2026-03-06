1ae9b151d77dba050116540da0d6c7f471cfd543
amdjobob <78293475+amdjobob@users.noreply.github.com>
2026-03-06 10:25:55 +0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0027-remove-element/0027-remove-element.cs
+++ 0027-remove-element/0027-remove-element.cs
@@ -1,5 +1,7 @@
 public class Solution {
     public int RemoveElement(int[] nums, int val) {
+        // 快慢指針，也叫讀寫指針
+        // 思路：當快指針不等於 val 時，寫入 array
         int slow = 0;
         for (int fast = 0; fast < nums.Length; fast++)
         {
