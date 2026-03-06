c76d809b17dd3fe1a0a9e89277f8e2667d1f6ef5
amdjobob <78293475+amdjobob@users.noreply.github.com>
2026-03-06 13:49:34 +0800

    Time: 0 ms (100%), Space: 53.4 MB (5.12%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cs
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cs
@@ -1,12 +1,15 @@
 public class Solution {
     public int MinSubArrayLen(int target, int[] nums) {
+        // 滑動窗口法，用一個迴圈控制兩個指標
+        // 思路：當 sum < target 時，增加範圍
+        //       直到 sum > target 時，減小範圍
         int left = 0, right = 0, sum = 0, length = nums.Length + 1;
-        for (right = 0; right < nums.Length; right++)
+        for (right = 0; right < nums.Length; right++) // 第一個迴圈控制終點
         {
             sum += nums[right];
             while(sum >= target)
             {
-                if (right - left + 1 < length) length = right - left + 1;
+                if (right - left + 1 < length) length = right - left + 1; // 縮小範圍
                 sum -= nums[left++];
                 if (sum < target) break;
             }
