4d231b5cca7c222ab9d580105bbee3a8c01152b0
amdjobob <78293475+amdjobob@users.noreply.github.com>
2026-03-06 13:35:49 +0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cs
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cs
@@ -13,28 +13,5 @@ public class Solution {
         }
         if (length == nums.Length + 1) return 0;
         return length;
-        // 暴力法，會造成 Time Limit Exceeded
-        // int subsum = 0, subnum = 0, result = nums.Length, find = 0;
-        // for (int i = 0; i < nums.Length; i++)
-        // {
-        //     subsum = 0;
-        //     subnum = 0;
-        //     for (int j = i; j < nums.Length; j++)
-        //     {
-        //         subsum += nums[j];
-        //         subnum += 1;
-        //         if (subsum >= target) 
-        //         {
-        //             if (subnum <= result)
-        //             {
-        //                 result = subnum;
-        //             }
-        //             find = 1;
-        //             break;
-        //         }
-        //     }
-        // }
-        // if (find == 0) return 0;
-        // return result;
     }
 }
