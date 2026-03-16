a840f126be60debd460b1728cf6e974e8bb50f76
amdjobob <78293475+amdjobob@users.noreply.github.com>
2026-03-16 14:15:24 +0800

    Time: 0 ms (100%), Space: 40.3 MB (26.54%) - LeetHub

--- 0059-spiral-matrix-ii/0059-spiral-matrix-ii.cs
+++ 0059-spiral-matrix-ii/0059-spiral-matrix-ii.cs
@@ -2,11 +2,10 @@ public class Solution {
     public int[][] GenerateMatrix(int n) {
         // 二維陣列初始化
         int[][] result = new int[n][];
-        for (int i = 0; i < n; i++)
-        {
-            result[i] = new int[n];
-        }
+        for (int i = 0; i < n; i++) { result[i] = new int[n]; }
 
+        // 解題思路：撞到邊邊就換方向。
+        // path 是方向  1右2下3左4上
         int path = 1, edge = 0, x = 0, y = 0;
         for (int i = 1; i < n*n+1; i++)
         {
@@ -23,12 +22,12 @@ public class Solution {
             if (path == 3)
             {
                 result[x][y--] = i;
-                if (y == -1 + edge) { path = 4; edge += 1; y += 1; }
+                if (y == -1 + edge) { path = 4; edge += 1; y += 1; } // 關鍵！要把邊邊給縮小
             }
             if (path == 4)
             {
                 result[x--][y] = i;
-                if (x == -1 + edge) { path = 1; x += 1; y += 1;}
+                if (x == -1 + edge) { path = 1; x += 1; y += 1;}    // 關鍵！最外圈結束，往內縮
             }
         }
         return result;
