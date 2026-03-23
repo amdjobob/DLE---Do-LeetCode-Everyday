d51e14bf0d9afce0cc52c0843d5eb530abe373d3
amdjobob <78293475+amdjobob@users.noreply.github.com>
2026-03-23 09:40:31 +0800

    Time: 166 ms (96.25%), Space: 69.9 MB (17.5%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cs
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cs
@@ -8,16 +8,17 @@
  */
 public class Solution {
     public ListNode GetIntersectionNode(ListNode headA, ListNode headB) {
+        // 思路：讓兩 linked-lists 從相同的長度出發
         ListNode A = headA, B = headB;
         int a = 0, b = 0;
-        while(A.next != null) { a += 1; A = A.next; }
-        while(B.next != null) { b += 1; B = B.next; }
+        while(A.next != null) { a += 1; A = A.next; } // 計算 A 的長度
+        while(B.next != null) { b += 1; B = B.next; } // 計算 B 的長度
         A = headA;
         B = headB;
-        if (a>b) for (int i = 0; i<a-b; i++) A = A.next;
-        else for (int i = 0; i<b-a; i++) B = B.next;
-        while(A != B && A.next != null) {A = A.next; B = B.next;}
-        if (A == B) return A;
+        if (a>b) for (int i = 0; i<a-b; i++) A = A.next; // 如果 A 比較長，讓 A 先走
+        else for (int i = 0; i<b-a; i++) B = B.next;     // 如果 B 比較長，讓 B 先走
+        while(A != B && A.next != null) {A = A.next; B = B.next;} // 兩個一起走，直到相同的節點
+        if (A == B) return A;  // 找到了，回傳相遇的點
         else return null;
     }
 }
