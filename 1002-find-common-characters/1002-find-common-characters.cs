e11d63d4fda698186b6300dc35f5fd8063b81dff
amdjobob <78293475+amdjobob@users.noreply.github.com>
2026-03-26 14:27:48 +0800

    Time: 4 ms (75.49%), Space: 49 MB (86.27%) - LeetHub

--- 1002-find-common-characters/1002-find-common-characters.cs
+++ 1002-find-common-characters/1002-find-common-characters.cs
@@ -1,24 +1,23 @@
 public class Solution {
     public IList<string> CommonChars(string[] words) {
         int[] count = new int[26];
-        int[] count2 = new int[26];
         foreach(char c in words[0]) { count[c - 'a'] += 1; }
         for (int i = 1; i < words.Length; i++)
         {
+            int[] count2 = new int[26];
             foreach(char c in words[i]) { count2[c - 'a'] += 1; }
             for(int j = 0; j < 26; j++)
             {
                 count[j] = count[j] > count2[j] ? count2[j] : count[j];
-                Console.Write(j);
             }
         }
         int num = 0;
         foreach(int i in count) if (i != 0) num += i;
         string[] result = new string[num];
-        // for(int j = 0; j < 26; j++)
-        // {
-        //     for (int i=0; i<j; i++) result[--num] = (j + 'a').ToString();
-        // }
+        for(int j = 0; j < 26; j++)
+        {
+            for (int i=0; i<count[j]; i++) result[--num] = ((char)(j + 'a')).ToString();
+        }
         return result;
     }
 }
