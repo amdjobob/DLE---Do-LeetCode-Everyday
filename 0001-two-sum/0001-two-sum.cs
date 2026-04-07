4df44532bafbd5f4eaf099afc7d4caa09aa0c916
amdjobob <78293475+amdjobob@users.noreply.github.com>
2026-04-07 09:43:26 +0800

    Time: 4 ms (73.54%), Space: 49.8 MB (29.61%) - LeetHub

--- 0001-two-sum/0001-two-sum.cs
+++ 0001-two-sum/0001-two-sum.cs
@@ -1,13 +1,13 @@
 public class Solution {
     public int[] TwoSum(int[] nums, int target) {
-        HashSet<int> hash = new HashSet<int>();
+        Dictionary<int, int> map = new Dictionary<int, int>();
         int[] result = new int[2];
         for (int i = 0; i<nums.Length; i++){
-            if (hash.Contains(target - nums[i])){
-                return [i, nums.IndexOf(target - nums[i])];
+            if (map.ContainsKey(target - nums[i])){
+                return [i, map[target - nums[i]]];
             }
             else{
-                hash.Add(nums[i]);
+                map[nums[i]] = i;
             }
         }
         return [];
