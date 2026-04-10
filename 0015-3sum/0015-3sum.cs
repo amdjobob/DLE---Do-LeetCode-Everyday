41dc06e518535ac94ee278b0e88db24a7897eb62
amdjobob <78293475+amdjobob@users.noreply.github.com>
2026-04-10 08:58:29 +0800

    Time: 35 ms (65.94%), Space: 66.2 MB (34.4%) - LeetHub

---
+++ 0015-3sum/0015-3sum.cs
@@ -0,0 +1,29 @@
+public class Solution {
+    public IList<IList<int>> ThreeSum(int[] nums) {
+        List<IList<int>> result = new List<IList<int>>();
+        int[] numsSorted = new int[nums.Length];
+        nums.CopyTo(numsSorted);
+        numsSorted.Sort();
+        int count = 0;
+        for(int i = 0; i<nums.Length-2; i++){
+            int left = i+1, right = nums.Length-1;
+            if(i!=0 && numsSorted[i] == numsSorted[i-1]) continue;
+            while(left < right){
+                int sum = numsSorted[i] + numsSorted[left] + numsSorted[right];
+                if (sum < 0){
+                    left++;
+                }
+                else if (sum > 0){
+                    right--;
+                    continue;
+                }
+                else{
+                    result.Add([numsSorted[i], numsSorted[left], numsSorted[right]]);
+                    left++;
+                }
+                while(numsSorted[left] == numsSorted[left-1] && left!=nums.Length-1) left++;
+            }
+        }
+        return result;
+    }
+}
